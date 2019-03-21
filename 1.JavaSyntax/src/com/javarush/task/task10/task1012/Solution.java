e6657b4862786caf0ccff90c3fd24d0d0fea6862
denisp <denis.pergamenstschik@cu-bx.com>
2019-03-21 14:40:40 +0200

    tasks till 1018

--- 1.JavaSyntax/src/com/javarush/task/task10/task1012/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1012/Solution.java
@@ -3,8 +3,8 @@ package com.javarush.task.task10.task1012;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /* 
 Количество букв
@@ -24,6 +24,7 @@ public class Solution {
         }
 
         // Ввод строк
+        //TODO return strings number to 10
         ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < 10; i++) {
             String s = reader.readLine();
@@ -31,20 +32,36 @@ public class Solution {
         }
 
         // напишите тут ваш код
-        Map<Character, Integer> output = new HashMap<>(33);
-        ArrayList<Character> characters = new ArrayList<>();
+        Map<Character, Integer> charsMap = new TreeMap<Character, Integer>(); //output map
+        ArrayList<Character> characters = new ArrayList<>(); // list to collect all characters from strings
 
+        //fori for enter characters to list
         for (String string : list) {
             for (int i = 0; i < string.length(); i++) {
                 characters.add(string.charAt(i));
             }
         }
-        //
 
-        System.out.println(characters.toString());
+        for (int i = 0; i < characters.size(); i++) {
+            Integer characterCount = charsMap.get(characters.get(i));
+            if (characterCount == null) {
+                charsMap.put(characters.get(i), 1);
+            } else {
+                int count = characterCount;
+                count++;
+                charsMap.put(characters.get(i), count);
+            }
+        }
 
-
-
+        for (int i = 0; i < alphabet.size(); i++) {
+            String s;
+            if (charsMap.get(alphabet.get(i)) != null) {
+                s = "" + alphabet.get(i) + " " + charsMap.get(alphabet.get(i));
+            } else {
+                s = "" + alphabet.get(i) + " 0"  ;
+            }
+            System.out.println(s);
+        }
     }
 
 }
